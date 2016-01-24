d6b30298500e4a9ba51a4249334a24e937bd8ca2
PraiseBeToScience <praisebetoscience@users.noreply.github.com[core]>
2016-01-24 16:11:50 -0500

    added requirements.txt

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
+from pip.req import parse_requirements
+import os
 
-exec(open("./package.py").read())
+HERE = os.path.abspath(os.path.dirname(__file__))
+exec(open(os.path.join(HERE, "package.py")).read())
+
+install_reqs = parse_requirements(os.path.join(HERE, "requirements.txt"))
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name=APPNAME,
       version=__version__,
       description=DESCRIPTION,
       author=__author__,
       url=URL,
-      requires=['Flask>=0.10.1', 'praw>=3.3.0', 'requests>=2.9.1'])
+      install_requires=reqs,
+      requires=reqs)
